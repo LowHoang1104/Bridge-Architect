4d7d8fc1046a0ebdd3a69664899ed1b68fcdf8cd
DESKTOP-C3NUS3R\Asus <longx3732@gmail.com>
2026-03-03 10:03:39 +0700

    done scene 1

---
+++ Bridge-Architect/Assets/Scripts/BarCreator.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BarCreator : MonoBehaviour, IPointerDownHandler
+{
+    public GameManager gameManager;
+
+    public GameObject roadBar;
+    public GameObject woodBar;
+
+    bool barCreationStarted = false;
+
+    public Bar currentBar;
+    public GameObject barToInstantiate;
+    public Transform barParent;
+
+    public Point currentStartPoint;
+    public Point currentEndPoint;
+    public GameObject pointToInstantiate;
+    public Transform pointParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (barCreationStarted)
+        {
+            Vector2 endPosition = (Vector2)Vector2Int.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            Vector2 dir = endPosition - currentBar.startPosition;
+            Vector2 clampedPosition = currentBar.startPosition + Vector2.ClampMagnitude(dir, currentBar.maxLength);
+
+            currentEndPoint.transform.position = (Vector2)Vector2Int.FloorToInt(clampedPosition);
+            currentEndPoint.pointId = currentEndPoint.transform.position;
+            currentBar.UpdateCreatingBar(currentEndPoint.transform.position);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!barCreationStarted)
+        {
+            barCreationStarted = true;
+            StartBarCreation(Vector2Int.RoundToInt(Camera.main.ScreenToWorldPoint(eventData.position)));
+        }
+        else
+        {
+            if(eventData.button == PointerEventData.InputButton.Left)
+            {
+                if(gameManager.CanBuyItem(currentBar.actualCost)) FinishBarCreation();
+            }
+            else if(eventData.button == PointerEventData.InputButton.Right)
+            {
+                barCreationStarted = false;
+                DeleteCurrentBar();
+            }
+        }
+    }
+
+    private void DeleteCurrentBar()
+    {
+        Destroy(currentBar.gameObject);
+        if (currentStartPoint.connectedBars.Count == 0 && currentStartPoint.runtime) Destroy(currentStartPoint.gameObject);
+        if (currentEndPoint.connectedBars.Count == 0 && currentEndPoint.runtime) Destroy(currentEndPoint.gameObject);
+    }
+
+    private void FinishBarCreation()
+    {
+        if (GameManager.AllPoints.ContainsKey(currentEndPoint.transform.position))
+        {
+            Destroy(currentEndPoint.gameObject);
+            currentEndPoint = GameManager.AllPoints[currentEndPoint.transform.position];
+        }
+        else
+        {
+            GameManager.AllPoints.Add(currentEndPoint.transform.position, currentEndPoint);
+        }
+
+        currentStartPoint.connectedBars.Add(currentBar);
+        currentEndPoint.connectedBars.Add(currentBar);
+
+        currentBar.startJoint.connectedBody = currentStartPoint.rb;
+        currentBar.startJoint.anchor = currentBar.transform.InverseTransformPoint(currentBar.startPosition);
+        currentBar.endJoint.connectedBody = currentEndPoint.rb;
+        currentBar.endJoint.anchor = currentBar.transform.InverseTransformPoint(currentEndPoint.transform.position);
+
+        gameManager.UpdateBudget(currentBar.actualCost);
+
+        StartBarCreation(currentEndPoint.transform.position);
+    }
+
+    private void StartBarCreation(Vector2 startPosition)
+    {
+        currentBar = Instantiate(barToInstantiate, barParent).GetComponent<Bar>();
+        currentBar.startPosition = startPosition;
+
+        if (GameManager.AllPoints.ContainsKey(startPosition))
+        {
+            currentStartPoint = GameManager.AllPoints[startPosition];
+        }
+        else
+        {
+            currentStartPoint = Instantiate(pointToInstantiate, startPosition, Quaternion.identity, pointParent).GetComponent<Point>();
+            GameManager.AllPoints.Add(startPosition, currentStartPoint);
+        }
+
+        currentEndPoint = Instantiate(pointToInstantiate, startPosition, Quaternion.identity, pointParent).GetComponent<Point>();
+    }
+}
---
+++ Bridge-Architect/Assets/Scripts/UIManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Button roadButton;
+    public Button woodButton;
+    public BarCreator barCreator;
+
+    public Slider budgetSlider;
+    public TextMeshProUGUI BudgetText;
+    public Gradient myGradient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roadButton.onClick.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("map1");
+    }
+
+    public void ChangeBar(int barType)
+    {
+        if(barType == 0)
+        {
+            roadButton.GetComponent<Outline>().enabled = true;
+            woodButton.GetComponent<Outline>().enabled = false;
+            barCreator.barToInstantiate = barCreator.roadBar;
+        }
+        if (barType == 1)
+        {
+            woodButton.GetComponent<Outline>().enabled = true;
+            roadButton.GetComponent<Outline>().enabled = false;
+            barCreator.barToInstantiate = barCreator.woodBar;
+        }
+    }
+
+    public void UpdateBudgetUI(float currentBudget, float levelBudget)
+    {
+        BudgetText.text = Mathf.FloorToInt(currentBudget).ToString() + "$";
+        budgetSlider.value = currentBudget / levelBudget;
+        budgetSlider.fillRect.GetComponent<Image>().color = myGradient.Evaluate(budgetSlider.value);
+
+    }
+}
