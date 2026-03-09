cb7cc5751b4ed603511d4bce1a049c93f16b52c1
DESKTOP-C3NUS3R\Asus <longx3732@gmail.com>
2026-03-09 11:31:39 +0700

    Done burn out budget and show stars

--- Bridge-Architect/Assets/Scripts/GameManager.cs
+++ Bridge-Architect/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,6 +9,7 @@ public class GameManager : MonoBehaviour
     public float currentBudget = 0;
     public UIManager myUIManager;
     public static Dictionary<Vector2, Point> AllPoints = new Dictionary<Vector2, Point>();
+    public bool winScene = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +32,7 @@ public class GameManager : MonoBehaviour
 
     public bool CanBuyItem(float itemCost)
     {
-        return itemCost < levelBudget;
+        return itemCost <= currentBudget;
     }
 
     public void UpdateBudget(float itemCost)
@@ -38,5 +40,37 @@ public class GameManager : MonoBehaviour
         currentBudget -= itemCost;
         myUIManager.UpdateBudgetUI(currentBudget, levelBudget);
     }
-    
+
+    public bool GameOver()
+    {
+        return currentBudget <= 0;
+    }
+
+    public void WinGame()
+    {
+        int achievedStars = GetStars();
+            Debug.Log("Stars: " + achievedStars);
+        myUIManager.ShowStars(achievedStars);
+        
+        winScene = true;
+    }
+
+    public int GetStars()
+    {
+        float usedBudget = levelBudget - currentBudget;
+        float onePart = levelBudget / 4f;
+
+        if (usedBudget <= 2f * onePart)
+        {
+            return 3;
+        }
+        else if (usedBudget <= 3f * onePart)
+        {
+            return 2;
+        }
+        else
+        {
+            return 1;
+        }
+    }
 }
